26d93361fb45e658c4d7c237c651ac35383905d7
Robson Quedevez de Alcantara <robson.quedevez@grupoceral.local>
2021-04-03 14:14:28 -0300

    Web: Added actions button in routes page

--- web/src/pages/Route/index.tsx
+++ web/src/pages/Route/index.tsx
@@ -1,19 +1,30 @@
 import React from 'react';
 
 import Navbar from '../../components/Navbar';
+import Map from '../../components/Map';
 
-import { Container } from './styles';
+import { Container, BtnAction } from './styles';
 
 const Route: React.FC = () => {
 
     return(
         <Container>
-            <Navbar />
-            <div style={{ width: '100%' }}>
-                <h1 style={{ textAlign:'center' }}>Routes</h1>
-            </div>
+            <Navbar />            
+            <Map />
+
+            <BtnAction>
+                <ul>
+                    <li><a className="btn-floating yellow" title='Criar Check Points'><i className="material-icons">push_pin</i></a></li>
+                    <li><a className="btn-floating red" title='Criar Quadrante'><i className="material-icons">highlight_alt</i></a></li>
+                    <li><a className="btn-floating green" title='Criar Rotas' ><i className="material-icons">place</i></a></li>
+                    <li><a className="btn-floating blue" title='Pesquisar Endereço'><i className="material-icons">search</i></a></li>
+                </ul>
+                <a className="btn-floating btn-large ">
+                    <i className="large material-icons">menu</i>
+                </a>
+            </BtnAction>
+            
         </Container>
     );
 }
-
 export default Route;
--- web/src/pages/Route/styles.ts
+++ web/src/pages/Route/styles.ts
@@ -7,3 +7,50 @@ export const Container = styled.div`
     align-items: center;
     flex-direction: row;
 `;
+
+export const BtnAction = styled.div`
+    position: absolute;
+    right: 50px;
+    bottom: 50px;
+    z-index: 999;
+    padding: 0px;
+
+    ul {
+        padding: 0px;
+        height: 0px;
+        width: 0px;
+        height: 20px;
+        opacity: 0;
+        transition: height 0.3s;
+
+        li {
+            position: absolute;
+            right: 10px;
+            bottom: 10px;
+        }
+        
+    }
+
+    &:hover {
+
+        ul {
+            display: flex;
+            flex-direction: column;
+            justify-content: space-around;
+            align-items: center;
+            padding: 0px;
+            margin: 0px 0px 10px 0px;
+            width: 100%;
+            height: 200px;
+            opacity: 1;
+
+            li {
+                position: static;
+                right: 10px;
+                bottom: 10px;
+            }
+
+            
+        }
+    }
+`;
