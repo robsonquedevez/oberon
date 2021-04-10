430d4a94d6cc283b01c1818e5eb0c088350b8f33
Robson Quedevez de Alcantara <robson.quedevez@grupoceral.local>
2021-04-10 19:40:47 -0300

    Web: Added marker when clicking on the map

--- web/src/components/Map/index.tsx
+++ web/src/components/Map/index.tsx
@@ -1,12 +1,37 @@
+
 import 'leaflet/dist/leaflet.css';
-import React from 'react';
-import { MapContainer, TileLayer } from 'react-leaflet';
+import React, { useState } from 'react';
+import { MapContainer, TileLayer, useMapEvents, Marker } from 'react-leaflet';
 import L from 'leaflet';
 
 import { Container } from './styles';
 
+import marker from '../../assets/marker.svg';
+
+const markerIcon = L.icon({
+    iconUrl: marker,
+    iconSize: [58, 58],
+})
+
+const AddMarker: React.FC = () => {
+    const  [position, setPosition] = useState<any>(null);
+
+    useMapEvents({
+        click: (e) => {
+            setPosition(L.latLng(e.latlng));
+            console.log(e);
+        }
+    });
+
+    return position === null ? null :
+    <Marker
+        position={position}
+        icon={markerIcon}
+    />
+};
+
 const Map: React.FC = () => {
-    const initialCoords = L.latLng(-22.7599758, -47.7007174)
+    const initialCoords = L.latLng(-22.7599758, -47.7007174)  
     
     return (
         <Container>
@@ -16,12 +41,16 @@ const Map: React.FC = () => {
                 scrollWheelZoom={true}
                 style={{ width: '100%', height: '100%' }}
                 zoomControl={false}
-
             >
                 <TileLayer
                     attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    
                 />
+
+                {
+                   <AddMarker />
+                }
             </MapContainer>
         </Container>
     );
