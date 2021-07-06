931ac8979489b3440fdba403450bce0c00c65684
Robson Quedevez de Alcantara <robson.quedevez@grupoceral.local>
2021-07-05 21:28:52 -0300

    Environment setting

---
+++ src/server.ts
@@ -0,0 +1,38 @@
+import 'dotenv/config';
+import express, { Request, Response, NextFunction, json } from 'express';
+import cors from 'cors';
+import routes from './routes';
+import AppErrors from './utils/errors/AppErrors';
+import log from './utils/log/LogsManager';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+app.use((
+    err: Error, 
+    request: Request, 
+    response: Response, 
+    next: NextFunction) => {
+
+    if(err instanceof AppErrors) {
+        return response
+        .status(err.statusCode)
+        .json({
+            status: 'error',
+            message: err.message
+        });
+    }
+
+    log.execute(err.message);
+
+    return response
+    .status(500)
+    .json({
+        status: 'error',
+        message: 'Internal server error',        
+    });
+});
+
+app.listen(4004);
