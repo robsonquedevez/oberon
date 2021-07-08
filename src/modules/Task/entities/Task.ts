5a27c6f560fa93a7f43d2c03dcdb12c12f47cbcb
Robson Quedevez de Alcantara <robson.quedevez@grupoceral.local>
2021-07-07 21:40:12 -0300

    Create entity to Task

---
+++ src/modules/Task/entities/Task.ts
@@ -0,0 +1,46 @@
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity('tasks')
+class Task {
+    
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column('interger')
+    type: number;
+
+    @Column()
+    created_user: string;
+
+    @Column()
+    enterprise: string;
+
+    @Column()
+    executing_task: string;
+
+    @Column('integer')
+    status_task: number;
+
+    @Column("time with time zone")
+    start_date: Date;
+
+    @Column('time with time zone')
+    end_date: Date;
+
+    @Column('boolean')
+    repeat: boolean;
+
+    @Column()
+    days_of_the_week: string;
+
+    @Column('boolean')
+    finished: boolean;
+
+    @CreateDateColumn()
+    created_at: Date;
+
+    @UpdateDateColumn()
+    updated_at: Date;
+}
+
+export default Task;
