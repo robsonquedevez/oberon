23cb7dcb3d58f954fe652d955cf61f64fe680366
Robson Quedevez de Alcantara <robson.quedevez@grupoceral.local>
2021-07-10 16:06:12 -0300

    Adding date to log record

--- src/utils/log/LogsManager.ts
+++ src/utils/log/LogsManager.ts
@@ -3,9 +3,11 @@ import fs from 'fs'
 class LogsManager {
 
     public static execute(payload: any): void {
+        const msg = Date() + ' - ' + payload;
+
         fs.writeFileSync(
-            '../../../errors-log.log',
-            payload+"\n",
+            'errors-log.log',
+            msg+"\n",
             {
                 encoding: 'utf-8',
                 flag: 'w+'
