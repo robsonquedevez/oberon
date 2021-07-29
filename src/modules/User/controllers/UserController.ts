7a6365ee5276e6d6c6b396d5034f11483505ab03
Robson Quedevez de Alcantara <robson.quedevez@grupoceral.local>
2021-07-28 21:18:32 -0300

    Changed update user and reset password include to in controller and route files

--- src/modules/User/controllers/UserController.ts
+++ src/modules/User/controllers/UserController.ts
@@ -4,6 +4,8 @@ import AppErrors from '../../../utils/errors/AppErrors';
 
 import CreateUserService from '../services/CreateUserService';
 import ListProfileService from '../services/ListProfileService';
+import UpdateUserService from '../services/UpdateUserService';
+import ResetPasswordService from '../services/ResetPasswordService';
 
 class UserController {
 
@@ -49,6 +51,44 @@ class UserController {
         delete user.password;
 
         return response.status(201).json({ user });
+    }
+
+    public async update(request: Request, response: Response): Promise <Response> {
+
+        const { id } = request.user;
+
+        const { name, administrator, enterprise } = request.body;
+        
+        const updateUser = new UpdateUserService();
+
+        const user = await updateUser.execute({
+            id,
+            name,
+            administrator,
+            enterprise
+        });
+
+        delete user.password;
+
+        return response.status(200).json({user});
+    }
+
+    public async changePassword(request: Request, response: Response): Promise <Response> {
+
+        const { id } = request.user;
+
+        const { currentPassword, newPassword, confirmationPassword } = request.body;
+
+        const resetPassword = new ResetPasswordService();
+
+        await resetPassword.execute({
+            id,
+            currentPassword,
+            newPassword,
+            confirmationPassword
+        });
+
+        return response.status(200).send();
     }
 }
 
--- src/modules/User/routes/user.routes.ts
+++ src/modules/User/routes/user.routes.ts
@@ -7,5 +7,7 @@ const userRouter = Router();
 
 userRouter.post('/', userController.create);
 userRouter.get('/', ensureAuthenticated, userController.findById);
+userRouter.put('/', ensureAuthenticated, userController.update);
+userRouter.patch('/', ensureAuthenticated, userController.changePassword)
 
 export default userRouter;
