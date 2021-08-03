c3877cc41f41f3b7f797ca3e79f35247178b2987
Robson Quedevez de Alcantara <robson.quedevez@grupoceral.local>
2021-08-03 15:47:41 -0300

    Add prefix router to coordinetes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,6 +4,7 @@ import userRouter from '../modules/User/routes/user.routes';
 import enterpriseRouter from '../modules/Enterprise/routes/enterprise.routes';
 import taskRouter from '../modules/Task/routes/task.routes';
 import sessionRouter from '../modules/User/routes/session.routes';
+import coordinateRouter from '../modules/Task/routes/coordinate.routes';
 
 const routes = Router();
 
@@ -11,5 +12,6 @@ routes.use('/session', sessionRouter);
 routes.use('/user', userRouter);
 routes.use('/enterprise', enterpriseRouter);
 routes.use('/task', taskRouter);
+routes.use('/coordinate', coordinateRouter);
 
 export default routes;
