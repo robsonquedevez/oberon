4509498b68fc108fdf1965d97e45b0445b73d66d
robson <robson@DEBIAN>
2021-08-29 15:10:06 -0300

    changed map component include children

--- app/src/components/Map/index.tsx
+++ app/src/components/Map/index.tsx
@@ -21,6 +21,9 @@ const Map: React.FC = ({ children }) => {
                 attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
+            {
+                children
+            }
             </MapContainer>
         </Container>
     );
