4aa8ff0bccb946b582be2af6298bc62b894dc2c0
robson <robson@DEBIAN>
2021-08-30 21:52:19 -0300

    Changed this page styles height

--- app/src/pages/Enterprise/index.tsx
+++ app/src/pages/Enterprise/index.tsx
@@ -1,17 +1,109 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import BaseNavbar from '../../components/BaseNavbar';
+import {
+    Paper,
+    Button,
+    CircularProgress,
+    makeStyles
+} from '@material-ui/core';
+import { Save } from '@material-ui/icons';
+import { FormHandles } from '@unform/core';
+import { Form } from '@unform/web';
+
+import Input from '../../components/Input';
 
 import {    
-    Container
+    Container,
+    Content,
+    InputGroup
 } from './styles';
 
+const useStyles = makeStyles((theme) => ({
+    paperContent: {
+        padding: 15,
+    }
+}));
+
 const Enterprise: React.FC = () => {
+    const classes = useStyles();
+    const formRef = useRef<FormHandles>(null);
+    const [btnLoading, setBtnLoading] = useState<boolean>(false);
 
     return (
         <BaseNavbar pageActive='enterprise'>
 
             <Container>
-                Entreprise
+
+                <Content>
+                 
+                    <h2> Empresa </h2>
+
+                    <Paper elevation={3} className={classes.paperContent}>
+
+                        <Form ref={formRef} onSubmit={() => {}}>
+
+                            <InputGroup>
+                                <Input
+                                    label='CNPJ'
+                                    name='cnpj'
+                                />
+                                <Input
+                                    label='Razão Social'
+                                    name='name'
+                                />                            
+                            </InputGroup>
+                            <InputGroup>
+                                <Input
+                                    label='E-mail'
+                                    name='email'
+                                />                            
+                                <Input
+                                    label='Endereço'
+                                    name='address'
+                                />
+                            </InputGroup>
+                            <InputGroup>
+                                <Input
+                                    label='Número'
+                                    name='number'
+                                />
+                                <Input
+                                    label='Bairro'
+                                    name='district'
+                                />
+                                <Input
+                                    label='Cidade'
+                                    name='city'
+                                />
+                                <Input
+                                    label='UF'
+                                    name='state'
+                                />
+                                <Input
+                                    label='CEP'
+                                    name='zip_code'
+                                />
+                            </InputGroup>
+
+                            <Button
+                                variant="contained"
+                                color='primary'
+                                type='submit'
+                                endIcon={
+                                    btnLoading ?
+                                    <CircularProgress color='inherit' size={18} />
+                                    : <Save />
+                                }
+                                disabled={btnLoading}
+                            >
+                                Salvar
+                            </Button>
+                        </Form>
+
+                    </Paper>
+
+                </Content>
+
             </Container>
 
         </BaseNavbar> 
--- app/src/pages/Enterprise/styles.ts
+++ app/src/pages/Enterprise/styles.ts
@@ -1,3 +1,35 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+    display: block;
+`;
+
+export const Content = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    padding: 1rem 3rem;    
+
+    h2 {
+        font-weight: 400;
+        font-size: 2rem;
+        font-family: 'Roboto', sans-serif;
+        margin-bottom: 1rem;
+        color: #566573;
+    }
+
+    button {
+        margin-top: 1.5rem;
+    }
+`;
+
+export const  InputGroup = styled.div`
+    display: flex;
+    justify-content: space-arrow;
+    align-items: center;
+
+    div + div {
+        margin-left: 1rem;
+    }
+`;
