2fde8c44d2e93b5a9f9a84b7ad8de0070e4b5ce5
Robson Qeudevez <robson.quedevez@hotmail.com>
2021-09-11 16:22:00 -0300

    Include news providers authenticated and notification message

--- app/src/hooks/index.tsx
+++ app/src/hooks/index.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import { MenuProvider } from './MenuContext';
+import { AuthProvider } from './Auth';
+import { SnackbarProvider } from 'notistack'
+  
 
 const AppProvider: React.FC = ({ children }) => (
-    <MenuProvider>
-        { children }
-    </MenuProvider>
+    <AuthProvider>
+        <MenuProvider>
+            <SnackbarProvider
+                anchorOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right'
+                }}
+            >
+                { children }
+            </SnackbarProvider>
+        </MenuProvider>
+    </AuthProvider>
 )
 
 export default AppProvider;
