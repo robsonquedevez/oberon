f2f6c8ed8009da45dfbb6505a07eeba0e72207d0
Robson Qeudevez <robson.quedevez@hotmail.com>
2021-10-17 15:26:19 -0300

    Created service for get task and coordinates to executing user today

---
+++ server/src/modules/Task/services/FindTaskTodayToExecuteUserService.ts
@@ -0,0 +1,51 @@
+import { Between, getRepository } from "typeorm";
+import AppErrors from "../../../utils/errors/AppErrors";
+import Task from "../entities/Task";
+import { format } from 'date-fns';
+
+interface IFindTaskToExecuteUserService {
+    id: string;
+}
+
+class FindTaskTodayToExecuteUserService {
+
+    public async execute({ id }: IFindTaskToExecuteUserService): Promise<Task[]>{
+
+        const taskRepository = getRepository(Task);
+
+        const currentDate = format(new Date(), 'yyyy-MM-dd');
+        const startBetween = currentDate + ' 00:00:00';
+        const endBetween = currentDate + ' 23:59:59';
+
+        const tasks = await taskRepository.find({
+            select:[
+                "id",
+                "title",
+                "type",
+                "enterprise",
+                "finished", 
+                "start_task",
+                "end_task",
+                "repeat", 
+                "days_of_the_week", 
+                "status_task",
+                "executing_user",
+                "created_user"
+            ],
+            where: {
+                executing_user: id,
+                start_task: Between(startBetween, endBetween)
+            }
+        });
+
+        if(tasks.length === 0) {
+            throw new AppErrors(
+                'Nenhuma tarefa encontrada para esse usuário',
+                401
+            );            
+        }
+        return tasks;
+    }
+}
+
+export default FindTaskTodayToExecuteUserService;
