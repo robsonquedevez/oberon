be62bda94a1ca3d156b495170d8f5e864bd79c49
Robson Qeudevez <robson.quedevez@hotmail.com>
2021-11-21 00:24:51 -0300

    Changed API host

--- app/src/services/api.ts
+++ app/src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const conn = axios.create({
-    baseURL: 'http://192.168.1.112:4004'
+    // baseURL: 'http://54.232.216.22:4004/'
+    baseURL: 'http://localhost:4004/'
 });
 
 export default conn;
