7b01f494325330502c35f539b8f22ea347c9e4e8
Robson Qeudevez <robson.quedevez@hotmail.com>
2021-11-21 00:24:10 -0300

    Changed API host

--- mobile_app/src/services/api.ts
+++ mobile_app/src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://192.168.1.112:4004'
+    //baseURL: 'http://54.232.216.22:4004/'
+    baseURL: 'http://192.168.1.112:4004/'
 });
 
 export default api;
